de6daf44b5dc8575231d6a8d2782acc88102a128
kyk911127 <kyk911127@gmail.com>
2022-08-30 14:02:23 +0900

    22.08.30 타이틀수정

--- create/src/App.js
+++ create/src/App.js
@@ -4,7 +4,7 @@ import styles from "./App.module.css";
 function App() {
   return (
     <div>
-      <h1 className={styles.title}>HI</h1>
+      <h1 className={styles.title}>PropTypes, 독립적CSS모듈</h1>
       <Button text={"클릭!!"} />
     </div>
   );
